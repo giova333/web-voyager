dc26512030cb664418a42413d21fae0c43af98f4
giova333 <aleksgladun4aws@gmail.com>
2024-05-27 14:20:58 +0300

    feat: Add possibility to configure different models

--- src/main/java/com/webvoyager/lmm/LMMRequestSender.java
+++ src/main/java/com/webvoyager/lmm/LMMRequestSender.java
@@ -1,19 +1,21 @@
 package com.webvoyager.lmm;
 
+import com.webvoyager.utils.ResourceLoader;
 import dev.langchain4j.data.message.ChatMessage;
 import dev.langchain4j.model.openai.OpenAiChatModel;
 import dev.langchain4j.model.openai.OpenAiChatModelName;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Properties;
 
+@Slf4j
 class LMMRequestSender {
 
     private final LMMInteractionLogger lmmInteractionLogger = new LMMInteractionLogger();
 
-    private final OpenAiChatModel model = OpenAiChatModel.builder()
-            .apiKey(System.getenv("OPENAI_API_KEY"))
-            .modelName(OpenAiChatModelName.GPT_4_VISION_PREVIEW)
-            .build();
+    private final OpenAiChatModel model = getOpenaiChatModel();
 
     String send(List<ChatMessage> chatMessages) {
         lmmInteractionLogger.logRequest(chatMessages);
@@ -23,6 +25,34 @@ class LMMRequestSender {
         lmmInteractionLogger.logResponse(response);
 
         return response.content().text();
+    }
+
+    private static OpenAiChatModel getOpenaiChatModel() {
+        var config = ResourceLoader.loadResource("model.properties");
+        var properties = parseProperties(config);
+
+        var modelType = (String) properties.get("lmm.model.type");
+
+        var modelName = switch (modelType) {
+            case "gpt-4o" -> OpenAiChatModelName.GPT_4_O;
+            case "gpt-4-vision-preview" -> OpenAiChatModelName.GPT_4_VISION_PREVIEW;
+            default -> throw new IllegalArgumentException("Unsupported model type: " + modelType);
+        };
+        log.info("Using OpenAI model: {}", modelName);
+
+        return OpenAiChatModel.builder()
+                .apiKey(System.getenv("OPENAI_API_KEY"))
+                .modelName(modelName)
+                .build();
+    }
+
+    private static Properties parseProperties(String config) {
+        var properties = new Properties();
+        Arrays.stream(config.split("\n"))
+                .map(line -> line.split("="))
+                .forEach(pair -> properties.setProperty(pair[0], pair[1]));
+
+        return properties;
     }
 
 }
